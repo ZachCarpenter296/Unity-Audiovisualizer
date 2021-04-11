88d383ac2217beb0001e77e13ce1ad01ad610a5b
ZachCarpenter296 <54647059+ZachCarpenter296@users.noreply.github.com>
2021-04-10 18:49:14 -0700

    started implementing color change options

---
+++ Audiovisualizer/Assets/_Scripts/colorChangeMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChangeMenu : MonoBehaviour
+{
+    public GameObject colorPanelUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!colorPanelUI.activeSelf)
+            {
+                colorPanelUI.SetActive(true);
+            }
+            else
+            {
+                colorPanelUI.SetActive(false);
+            }
+        }
+    }
+}
